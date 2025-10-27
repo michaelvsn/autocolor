1671b994a62d299d9074e5c4981807a5055aadf7
michaelvsn <mikaelconnections@gmail.com>
2025-10-27 14:38:37 +0200

    Update michael-autocolor.control.js

--- michael-autocolor.control.js
+++ michael-autocolor.control.js
@@ -1,19 +1,31 @@
 loadAPI(20);
 
-host.defineController("michael.vision", "autocolor :) 0.2-beta", "0.2-beta", "cd1a2c22-b201-4dbf-a2c2-76fad81e38ed", "michael.vision");
+host.defineController("michael.vision", "autocolor :) 0.3-beta", "0.3-beta", "cd1a2c22-b201-4dbf-a2c2-76fad81e38ed", "michael.vision");
 
 // maximum number of tracks the full rescan function can see. increase this if you have more tracks in your projects.
 const MAX_TRACKS = 999;
 
-let cursorTrack;
 let projectTrackBank;
 let parentTracks = [];
+let isPaused = false;
 
 function init() {
     const preferences = host.getPreferences();
-
     preferences.getSignalSetting("keeps track colors matched with their group", "about", "");
-	preferences.getSignalSetting("github: @michaelvsn", "about", "");
+    preferences.getSignalSetting("github: @michaelvsn", "about", "");
+
+    const ioPanel = host.getDocumentState();
+    const pauseButton = ioPanel.getSignalSetting("pause for 15s to prevent Undo conflicts", "autocolor :)", "pause");
+
+    pauseButton.addSignalObserver(function() {
+        isPaused = true;
+        host.showPopupNotification("autocolor :) paused for 15 seconds");
+        host.scheduleTask(function() {
+            isPaused = false;
+            host.showPopupNotification("autocolor :) is back on!");
+            recolorAllTracks();
+        }, 15000);
+    });
 
     projectTrackBank = host.getProject().getRootTrackGroup().createTrackBank(MAX_TRACKS, 0, 0, true);
     for (let i = 0; i < MAX_TRACKS; i++) {
@@ -28,7 +40,7 @@ function init() {
     }
     projectTrackBank.itemCount().markInterested();
 
-    cursorTrack = host.createCursorTrack("michael_CursorTrack", "Selected Track", 0, 0, true);
+    const cursorTrack = host.createCursorTrack("michael_CursorTrack", "Selected Track", 0, 0, true);
     cursorTrack.position().markInterested();
     cursorTrack.position().addValueObserver(function() {
         host.scheduleTask(recolorAllTracks, 0);
@@ -38,6 +50,10 @@ function init() {
 }
 
 function recolorAllTracks() {
+    if (isPaused) {
+        return;
+    }
+
     const totalTracks = projectTrackBank.itemCount().get();
     for (let i = 0; i < totalTracks; i++) {
         colorTrackIfInGroup(projectTrackBank.getItemAt(i), parentTracks[i]);
@@ -47,9 +63,12 @@ function recolorAllTracks() {
 function colorTrackIfInGroup(track, parent) {
     if (!parent.exists().get()) return;
     const parentName = parent.name().get();
+
     if (parentName !== "Project" && !track.isGroup().get()) {
-        const parentColor = parent.color().get();
-        track.color().set(parentColor.getRed(), parentColor.getGreen(), parentColor.getBlue());
+        const currentColor = track.color().get();
+        if (currentColor.getAlpha() !== 0) {
+            track.color().set(0, 0, 0, 0);
+        }
     }
 }
 
